3767ce5fee01119a9a406ddd07f3bc3371e6b884
Tsheden Tshokey <tshedwork@gmail.com>
2025-09-26 08:36:43 +0000

    Create card component

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,29 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+  // Pass through static files
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("admin");
+
   // Reusable date filter
   eleventyConfig.addFilter("date", (dateObj, format = "yyyy") => {
+    // If no date provided, use current date
+    if (!dateObj) {
+      dateObj = new Date();
+    }
     return DateTime.fromJSDate(dateObj).toFormat(format);
   });
 
+  // Eleventy directory config
   return {
     dir: {
       input: "src",
       includes: "_includes",
       output: "_site"
-    }
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
   };
 };
